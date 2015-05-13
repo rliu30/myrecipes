30f66a9d149cf426ab5a3ea4ba7ddf28d1eb647b
rliu30 <lrp924@hotmail.com>
2015-05-13 17:16:20 -0400

    update css style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   	!!current_user
   end
 
-   def require_user
+  def require_user
     if !logged_in?
       flash[:danger] = "You must be logged in to perform that action"
       redirect_to recipes_path
